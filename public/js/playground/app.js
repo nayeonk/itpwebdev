2b33a1c73408f5411726336cbc1f83c07d1ae17c
David Tang <dtang859@gmail.com>
2013-12-07 00:34:46 -0800

    playground libraries

--- public/js/playground/app.js
+++ public/js/playground/app.js
@@ -1,5 +1,25 @@
 (function(window, undefined) {
 
+var libraries = {
+	jquery: {
+		src: 'http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js',
+		type: 'js'
+	},
+	underscore: { 
+		src: 'http://documentcloud.github.io/underscore/underscore-min.js',
+		type: 'js'
+	},
+	handlebars: { 
+		src: 'http://cdnjs.cloudflare.com/ajax/libs/handlebars.js/1.0.0/handlebars.js',
+		type: 'js'
+	},
+	backbone: [
+		'jquery',
+		'underscore',
+		{ src: 'http://documentcloud.github.io/backbone/backbone-min.js', type: 'js' }
+	]
+};
+
 var myTextarea = document.querySelector('textarea');
 var frame = document.querySelector('#preview');
 
@@ -42,10 +62,58 @@ var insertHTMLDocumentIntoFrame = function(frame, doc) {
   return destDocument;
 };
 
+var insertScript = function(html, script) {
+	var newHTML = script + "\n</body>";
+	return html.replace('</body>', newHTML);
+};
+
+var createScriptTag = function(src) {
+	return '<script src="' + src + '"></script>';
+};
+
+var createExternalStylesheet = function(href) {
+	return '<link rel="stylesheet" href="'+ href +'" />';
+};
+
+var createDependency = function(val) {
+	if (typeof val === 'string') {
+		if (libraries.hasOwnProperty(val)) {
+			return createScriptTag(libraries[val].src);
+		} else {
+			throw new Error('Missing dependency in libraries object.');
+		}
+	} else {
+		return createScriptTag(val.src);
+	}
+};
+
+var createScriptTagsForLibrary = function(library) {
+	var lib = libraries[library];
+	var scripts = [];
+
+	if ($.isPlainObject(lib)) {
+		return createScriptTag(lib.src);
+	}
+
+	lib.forEach(function(val) {
+		scripts.push(createDependency(val));
+	});
+
+	return scripts.join("\n");
+};
+
 editor.on('change', function() {
 	var html = editor.getValue();
 	var doc = createHTMLDocument(html);
 	insertHTMLDocumentIntoFrame(frame, doc);
+});
+
+$('#libraries').on('change', function() {
+	var lib = this.value;
+	var scripts = createScriptTagsForLibrary(lib);
+	var newHTML = insertScript(editor.getValue(), scripts);
+
+	editor.setValue(newHTML);
 });
 
 })(window);
